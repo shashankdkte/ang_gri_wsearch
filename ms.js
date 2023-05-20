bfb3e356439a9e0010b8facb953fb3355b493084
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-20 12:52:43 +0530

    add rxjs operator

--- ms.js
+++
@@ -1,3 +0,0 @@
-let html = "<h1>Hi There</h1>";
-html = html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-document.body.innerHTML = html;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { WikipediaService } from './wikipedia.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  pages = [];
+  pages: Array<any> = [];
   constructor(private wikipedia: WikipediaService) {}
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
     });
   }
 }
